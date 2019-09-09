5ae619553f363cc07ae661aed5051ee701646956
qinbb <qinbb@mail.jj.cn>
2019-09-09 18:22:02 +0800

    hello triangle

---
+++ LearnOpenGL/Shader.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include<GL/glew.h>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+
+using namespace std;
+class Shader
+{
+public:
+	GLuint Program;
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	void Use();
+};
+
